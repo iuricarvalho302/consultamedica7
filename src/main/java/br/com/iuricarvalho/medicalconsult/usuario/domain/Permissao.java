7d0f1a042b1cd34d1ebef6b302316c7a1324aba4
iuricarvalho302 <iuricarvalho302@gmail.com>
2024-10-14 18:04:57 -0300

    consulta7

---
+++ src/main/java/br/com/iuricarvalho/medicalconsult/usuario/domain/Permissao.java
@@ -0,0 +1,15 @@
+package br.com.iuricarvalho.medicalconsult.usuario.domain;
+
+public enum Permissao {
+    ADMIN("ADMIN"),
+    SECRETARIO("SECRETARIO");
+
+    private final String descricao;
+
+    Permissao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+}
